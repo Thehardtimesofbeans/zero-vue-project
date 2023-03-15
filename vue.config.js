efeb0c84632b0cfe191e9f31eca00b07bb14c3fb
huirong.guo <huirong.guo@doctopia.com.cn>
2023-03-15 18:28:17 +0800

    单元测试

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  chainWebpack(config) {
+    config.plugin('circular').use(new CircularDependencyPlugin())
+  },
+})
