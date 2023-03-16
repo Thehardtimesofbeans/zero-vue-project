2dc76489b833eb96f74c5e13bb48c7db5a4ab8da
huirong.guo <huirong.guo@doctopia.com.cn>
2023-03-16 14:42:47 +0800

    单元测试

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -12,13 +12,52 @@ const HelloWorld = defineComponent({
     }
   },
 })
+// 预设
+beforeEach(() => {
+  console.log('before each')
+})
+
+// 清理
+afterEach(() => {
+  console.log('after each')
+})
+
+beforeAll(() => {
+  console.log('before all')
+})
+
+afterAll(() => {
+  console.log('after all')
+})
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders props.msg when passed', async () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg },
     })
-    expect(wrapper.text()).toMatch(msg)
+
+    // setTimeout(() => {
+    //   expect(wrapper.text()).toMatch(msg)
+    //   done()
+    // }, 100)
+
+    // return new Promise<void>((resolve) => {
+    //   expect(wrapper.text()).toEqual(msg)
+    //   resolve()
+    // })
+
+    await wrapper.setProps({
+      msg: '123',
+    })
+    expect(wrapper.text()).toEqual('123')
+
+    // expect(wrapper.text()).toBe(msg) // Object.js ，值一样，比如是object，引用对象地址都一样
+
+    // expect(wrapper.text()).toEqual(msg)
+  })
+
+  it('should work', () => {
+    expect(1 === 1).toEqual(true)
   })
 })
